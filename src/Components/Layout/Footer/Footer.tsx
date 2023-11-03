64e702e478a69a08378be1580398410cab94a236
Barak1998 <barakm25@gmail.com>
2023-11-03 17:58:18 +0200

    first commit

---
+++ src/Components/Layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer(): JSX.Element {
+    return (
+        <div className="Footer">
+        <h2>All rights reseved &copy; to zeev Mother in Law</h2>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/Components/Layout/Header/Header.tsx
@@ -0,0 +1,29 @@
+import { subscribe } from "diagnostics_channel";
+import "./Header.css";
+import { useEffect, useState } from "react";
+import { youtube } from "../../Redux/Store";
+
+function Header(): JSX.Element {
+    const [totalSongs, setTotal] = useState(
+        youtube.getState().songs.allSongs.length
+    );
+    const [totalCategories, setCategoriesTotal] = useState(
+        youtube.getState().category.categories.length
+    );
+
+    youtube.subscribe(() => {
+        setTotal(youtube.getState().songs.allSongs.length);
+        setCategoriesTotal(youtube.getState().category.categories.length);
+    });
+
+    return (
+        <div className="Header">
+        <h1>Class 92</h1>
+        total songs: {totalSongs}
+        <br />
+        total categories:{totalCategories}
+        </div>
+    );
+}
+
+export default Header;
