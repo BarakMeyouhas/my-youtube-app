61d7899568723267730ff9f3fbfb86273043ca6a
Barak1998 <barakm25@gmail.com>
2023-11-04 12:15:01 +0200

    search song updated

--- src/Components/Pages/SongSearch/SongSearch.tsx
+++ src/Components/Pages/SongSearch/SongSearch.tsx
@@ -1,9 +1,52 @@
-import "./SongSearch.css";
+import { Button, TextField } from '@mui/material';
+import Song from '../../modal/Song';
+import { youtube } from '../../Redux/Store';
+import { SetStateAction, useState } from 'react';
+import SingleItem from '../AddSongForm/SingleItem/SingleItem';
 
 function SongSearch(): JSX.Element {
+    const allSongs = youtube.getState().songs.allSongs;
+    const [searchValue, setSearchValue] = useState('');
+    const [filteredSongs, setFilteredSongs] = useState<Song[]>(allSongs);
+
+    const handleSearch = () => {
+        const filtered = allSongs.filter(song =>
+            song.title.toLowerCase().includes(searchValue.toLowerCase())
+        );
+        setFilteredSongs(searchValue ? filtered : []);
+    };
+
+    const handleInputChange = (e: { currentTarget: { value: SetStateAction<string>; }; }) => {
+        setSearchValue(e.currentTarget.value);
+    };
+
     return (
         <div className="SongSearch">
-			<h1>Search Song</h1><hr />
+            <h1>Search Song</h1>
+            <TextField
+                label="Search Song"
+                variant="outlined"
+                className="SearchInput"
+                value={searchValue}
+                onChange={handleInputChange}
+            />
+            <Button variant="contained" color="primary" onClick={handleSearch}>
+                Search
+            </Button>
+            <hr />
+
+            <div>
+                {filteredSongs.map(song => (
+                    <SingleItem
+                    key={song["id"]}
+                    url={song["url"]}
+                    title={song["title"]}
+                    description={song["description"]}
+                    img={song["img"]}
+                    id={song["id"]}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+
 import reportWebVitals from "./reportWebVitals";
 import MainLayout from "./Components/Layout/MainLayout/MainLayout";
 import { BrowserRouter } from "react-router-dom";
