83870817ff10eac3525e6d75913da1b69c558116
Barak1998 <barakm25@gmail.com>
2023-11-21 11:21:43 +0200

    footer updates

--- Frontend/src/Components/Layout/Footer/Footer.tsx
+++ Frontend/src/Components/Layout/Footer/Footer.tsx
@@ -1,11 +1,50 @@
-import "./Footer.css";
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Link from "@mui/material/Link";
 
-function Footer(): JSX.Element {
-    return (
-        <div className="Footer">
-        <h2>All rights reseved &copy; to Barak Meyouhas</h2>
-        </div>
-    );
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {"Copyright © "}
+      <Link color="inherit" href="https://github.com/BarakMeyouhas">
+        Barak Meyouhas
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
 }
 
-export default Footer;
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+export default function StickyFooter() {
+    return (
+        <Box
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === "light"
+                ? theme.palette.grey[300]
+                : theme.palette.grey[900],
+            p: 4,
+            marginTop: 0,
+          }}
+          component="footer"
+        >
+          <Container maxWidth="sm">
+            <Typography variant="body2" color="text.secondary" align="center">
+              {"Copyright © "}
+              <Link color="inherit" href="https://your-website.com/">
+                Your Website
+              </Link>{" "}
+              {new Date().getFullYear()}
+              {"."}
+            </Typography>
+          </Container>
+        </Box>
+      );
+}
